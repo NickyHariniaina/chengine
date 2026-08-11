a71c8ff7d46e06978f8c448a465329a82b16be22
NickyHariniaina <ferrissushi@gmail.com>
2026-08-11 15:27:36 +0300

    fix: import printf

--- src/bitboard.c
+++ src/bitboard.c
@@ -1,16 +1,12 @@
 #include "include/bitboard.h"
 #include "include/types.h"
 #include <stdint.h>
+#include <stdio.h>
 
 const uint8_t MAX_BOARD_SIZE = 8;
 
-// Usage of population count here so we can count bit at 1 faster. I think it's
-// kinda obvious
 int count_bits(bitboard_size board) { return __builtin_popcountll(board); }
 
-// The fact is that we count like 0000_0001 and have h -> a because that's how
-// it work to be honest
-// So doing 0x1 << 3 will resolve in going to the d file
 int pop_least_significant_bit(bitboard_size *board) {
   int square = __builtin_ctzll(*board);
   *board &= *board - 0b00000001;
